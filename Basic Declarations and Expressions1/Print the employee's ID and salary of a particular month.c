6117a590f0a48a24a8d110304f39d1515412e034
Haffatun Jannat Shifa <125866170+Haffatun@users.noreply.github.com>
2024-01-01 23:58:49 +0600

    Create Print the employee's ID and salary of a particular month.c

---
+++ Basic Declarations and Expressions1/Print the employee's ID and salary of a particular month.c
@@ -0,0 +1,25 @@
+int main() {
+    char id[10];         
+    int hour;            
+    double value, salary; 
+
+    
+    printf("Input the Employees ID: ");
+    scanf("%s", &id);
+
+    
+    printf("\nInput the working hrs: ");
+    scanf("%d", &hour);
+
+    
+    printf("\nSalary amount/hr: ");
+    scanf("%lf", &value);
+
+    
+    salary = value * hour;
+
+    
+    printf("\nEmployees ID = %s\nSalary = U$ %.2lf\n", id, salary);
+
+    return 0;
+}
